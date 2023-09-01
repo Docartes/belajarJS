50c5f72c36c3e154d9cd2193bdcf843f8a3ce665
Docartes <129630064+Docartes@users.noreply.github.com>
2023-09-01 16:49:57 +0700

    belajar

---
+++ dasar1.js
@@ -0,0 +1,30 @@
+let a = 'Ahmad'; // string
+var b = 12; // integer
+const c = true; // bolean
+
+console.log(a, b, c);
+
+let angka1 = 12;
+let angka2 = 12;
+
+// operator : +, -, *, /, %
+console.log(angka1 + angka2 + " Penjumlahan");
+console.log(angka1 -angka2 + " Pengurangan");
+console.log(angka1 * angka2 + " Perkalian");
+console.log(angka1 / angka2 + " Pembagian");
+console.log(angka1 % angka2 + " Modulus / atau sisa pembagian");
+
+let hasil = angka1 + angka2;
+console.log(hasil);
+
+hasil = angka1 - angka2;
+console.log(hasil);
+
+hasil = angka1 * angka2;
+console.log(hasil);
+
+hasil = angka1 / angka2;
+console.log(hasil);
+
+hasil = angka1 % angka2;
+console.log(hasil);
