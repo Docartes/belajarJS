0b02186876bbf592d0cc653c4fae97426f0cf14c
Docartes <ahmadv0101@gmail.com>
2023-09-02 23:09:27 +0700

    Quiz part 8.4

---
+++ part8.4/soal4.js
@@ -0,0 +1,21 @@
+function pasanganTerbesar(num) {
+  // you can only write your code here!
+  let strNum = num.toString();
+  let terbesar = 0;
+
+  for (let i = 0; i < str.length - 1; i++) {
+    const pasangan = parseInt(strNum[i] + strNum[i + 1]);
+    if (pasangan > terbesar) {
+      terbesar = pasangan;
+    }
+  }
+
+  return temp;
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
